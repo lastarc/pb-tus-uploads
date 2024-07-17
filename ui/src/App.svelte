9b6da96d38a383ce732118e3b815c6aaf5463b3c
Arc <90333108+lastarc@users.noreply.github.com>
2024-07-18 01:31:47 +0200

    fix: clear authStore when stale

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -119,6 +119,10 @@
         }).then(uploadHistory.set)
 
     onMount(() => {
+        if (!pb.authStore.isValid) {
+            pb.authStore.clear()
+        }
+
         user.set(pb.authStore.model)
 
         const _redirectUrl = new URL(location.href)
